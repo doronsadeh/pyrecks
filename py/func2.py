22ed9158add3da8391cbb52751180365615a8e16
doron <doron@getshine.com>
2016-11-26 05:50:07 -0800

    import example

--- py/func2.py
+++ py/func2.py
@@ -1,8 +1,10 @@
+import time
 def func2(x):
     print 'func2 got %s' % x
+    time.sleep(10)
     x = x + '-and-we-are-done'
     print 'func2 says %s' % x
     return ''
 
 # Reduced version
-# SET func2 "def func2(x):\n\tprint 'func2 got %s' % x\n\tx = x + '-and-we-are-done'\n\tprint 'func2 says %s' % x\n\treturn ''\n"
+# SET func2 "import time \ndef func2(x):\n\tprint 'func2 got %s' % x\n\ttime.sleep(10)\n\tx = x + '-and-we-are-done'\n\tprint 'func2 says %s' % x\n\treturn ''"
